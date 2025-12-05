f50fdd5d494f6eb7c82638a4d984ad648fc1fc32
LIXIRI <tfs.lixiri@gmail.com>
2025-12-05 20:15:06 +0300

    path troubleshooting

--- training/char/train.py
+++ training/char/train.py
@@ -2,7 +2,7 @@ import os
 import sys
 import torch
 from torch.utils.data import DataLoader
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 from config_tiny import *
 import config_tiny
 from utils.char.tokenizer import SimpleCharTokenizer
